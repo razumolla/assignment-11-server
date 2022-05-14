760534e0642fcfe973098a02713c4f626cce5359
razumolla <rajuahmed9344@gmail.com>
2022-05-14 13:27:08 +0600

    add new product

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = 5000
 
 app.use(cors())
@@ -10,12 +11,32 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.phe4r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('Db Connected');
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connect');
+
+        const productCollection = client.db("carDealer").collection("products");
+
+        app.post("/addpd", async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
